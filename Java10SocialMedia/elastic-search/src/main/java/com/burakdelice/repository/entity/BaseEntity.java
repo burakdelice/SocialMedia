3c9caec3e67e0d8870a6b0b2beee70ad386c7fd4
Burak Delice <burakdelice.71@gmail.com>
2023-09-12 02:26:11 +0300

    SocialMedia GCloud Kubernetes
    12.09.2023

--- Java10SocialMedia/elastic-search/src/main/java/com/burakdelice/repository/entity/BaseEntity.java
+++ Java10SocialMedia/elastic-search/src/main/java/com/burakdelice/repository/entity/BaseEntity.java
@@ -1,11 +1,8 @@
 package com.burakdelice.repository.entity;
 
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
 import java.io.Serializable;
 
 @AllArgsConstructor
